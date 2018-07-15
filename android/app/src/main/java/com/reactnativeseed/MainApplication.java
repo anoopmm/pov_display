aac5d675881f803231745d0b7dfa9fd3f0a065b9
Anoop M M <anoopmmkt@gmail.com>
2018-07-15 23:42:59 +0530

    bluetooth added

--- android/app/src/main/java/com/reactnativeseed/MainApplication.java
+++ android/app/src/main/java/com/reactnativeseed/MainApplication.java
@@ -3,6 +3,9 @@ package com.reactnativeseed;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rusel.RCTBluetoothSerial.RCTBluetoothSerialPackage;
+import it.innove.BleManagerPackage;
+import com.polidea.reactnativeble.BlePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RCTBluetoothSerialPackage(),
+            new BleManagerPackage(),
+            new BlePackage(),
             new VectorIconsPackage()
       );
     }
